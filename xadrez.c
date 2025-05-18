aea57234ebae85ad714a9b566367d83d6046d2e1
Felipe Franco Mendes <114618103+Rokkondill@users.noreply.github.com>
2025-05-18 15:34:47 +0000

    Parte 1

--- xadrez.c
+++ xadrez.c
@@ -4,19 +4,36 @@
 // Este código inicial serve como base para o desenvolvimento do sistema de movimentação das peças de xadrez.
 // O objetivo é utilizar estruturas de repetição e funções para determinar os limites de movimentação dentro do jogo.
 
+
 int main() {
     // Nível Novato - Movimentação das Peças
     // Sugestão: Declare variáveis constantes para representar o número de casas que cada peça pode se mover.
 
+    const int bi = 5, to = 5, ra = 8;
+    int i;
+
+
     // Implementação de Movimentação do Bispo
     // Sugestão: Utilize uma estrutura de repetição para simular a movimentação do Bispo em diagonal.
-
+    printf("\nBispo cinco casas na diagonal direita para cima:\n");
+    for(i=1; i <= bi; i++){
+        printf("Cima, Direita");
+        printf(i!=bi ? " -> " : "\n");
+    }
     // Implementação de Movimentação da Torre
     // Sugestão: Utilize uma estrutura de repetição para simular a movimentação da Torre para a direita.
-
+    printf("\nTorre cinco casas a direita:\n");
+    for(i=1; i <= to; i++){
+        printf("Direita");
+        printf(i!=to ? " -> " : "\n");
+    }
     // Implementação de Movimentação da Rainha
     // Sugestão: Utilize uma estrutura de repetição para simular a movimentação da Rainha para a esquerda.
-
+    printf("\nRainha oito casas a esquerda:\n");
+    for(i=1; i <= ra; i++){
+        printf("Esquerda");
+        printf(i!=ra ? " -> " : "\n");
+    }
     // Nível Aventureiro - Movimentação do Cavalo
     // Sugestão: Utilize loops aninhados para simular a movimentação do Cavalo em L.
     // Um loop pode representar a movimentação horizontal e outro vertical.
@@ -27,6 +44,6 @@ int main() {
 
     // Sugestão: Implemente a movimentação do Cavalo utilizando loops com variáveis múltiplas e condições avançadas.
     // Inclua o uso de continue e break dentro dos loops.
-
+    printf("\n");
     return 0;
 }
